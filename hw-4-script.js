4b04ffac78a9430e8eec2754d4668d5f222106e5
Denver <cudedenver@gmail.com>
2025-10-28 11:58:15 -0700

    feat: finish section 1 exercises

---
+++ hw-4-script.js
@@ -0,0 +1,30 @@
+// Section 1: Basic Rules and Data Types
+
+// Excercise 1: Basic Rules and Data Types
+var thisIsANewVariable = 10;
+
+// using == to check value but NOT data type, === checks both
+if(thisIsANewVariable == 10){
+    console.log("thisIsANewVariable = " + thisIsANewVariable);
+}
+
+// Excercise 2: Defining Core Data Types
+let myString = "This is a string.";
+
+let myFloat = 22.7;
+
+let myBoolean = true;
+
+// Check values and types
+console.log("myString = " + myString + " " + typeof myString);
+console.log("myFloat = " + myFloat + " " + typeof myFloat);
+console.log("myBoolean = " + myBoolean + " " + typeof myBoolean)
+
+// Excercise 3: Single-Line Comments
+
+// Declares a variable named currentScore with the value 95.
+let currentScore = 95;
+
+// Exercise 4: Equality Comparison
+console.log("100" == 100);
+console.log("100" === 100);
